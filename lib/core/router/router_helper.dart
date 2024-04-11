320267c9ebd2eff0abceda6a762a183d7d47f010
Mohamed Gamal <mhammadgamal17@gmail.com>
2024-04-11 11:12:07 +0200

    add important routes

--- lib/core/router/router_helper.dart
+++ lib/core/router/router_helper.dart
@@ -1,3 +1,6 @@
 abstract class RouterHelper {
-
+  static const onBoarding = '/onBoarding';
+  static const login = '/login';
+  static const register = '/register';
+  static const homeScreen = '/homeScreen';
 }
