313e61473a23faac03818eb6d45b53aca111736f
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2024-07-01 14:18:55 +0300

    add fastlane ci to use firebase app_distribution

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppRouter {
   static final Map<String, WidgetBuilder> generateRoute = {
-
+    
   };
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:e_commerce/core/utils/localization/app_localization.dart';
 import 'package:e_commerce/e_commerce.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await init();
   await sl<AppLanguage>().fetchLocale();
   runApp(const ECommerceApp());
 }
