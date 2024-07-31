700a7a3036a8f9184ee680ed4489a9f990038367
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-31 15:18:03 +0300

    add/remove product to/from wishlist widget

---
+++ lib/features/home/presentation/widgets/wish_list_icon_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class WishListIcon extends StatelessWidget {
+  const WishListIcon(
+      {super.key, required this.isFavourite, required this.onFavPressed});
+
+  final bool isFavourite;
+  final void Function(int, bool) onFavPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 45.0,
+      height: 45.0,
+      child: Card(
+        color: Colors.white,
+        elevation: 1.5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            10.0,
+          ),
+          // side: const BorderSide(
+          //   width: 35.0,
+          // ),
+        ),
+        child: IconButton(
+          icon: Icon(
+            isFavourite ? Icons.favorite : Icons.favorite_border,
+            color: Colors.black,
+          ),
+          onPressed: () => onFavPressed,
+        ),
+      ),
+    );
+  }
+}
