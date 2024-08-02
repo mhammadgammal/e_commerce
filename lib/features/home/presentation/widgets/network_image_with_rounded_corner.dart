1a20777d6e9b43c1d14c3a6ff26b4ad77998b645
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-02 11:03:23 +0300

    add loading builder

--- lib/features/home/presentation/widgets/network_image_with_rounded_corner.dart
+++ lib/features/home/presentation/widgets/network_image_with_rounded_corner.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce/core/widgets/shimmer_loading.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../core/utils/screen_utils.dart';
 
 class NetworkImageWithRoundedCorner extends StatelessWidget {
   const NetworkImageWithRoundedCorner({super.key, required this.imageUrl});
@@ -12,7 +15,17 @@ class NetworkImageWithRoundedCorner extends StatelessWidget {
         child: ClipRRect(
           // Added ClipRRect
           borderRadius: BorderRadius.circular(10.0),
-          child: Image.network(fit: BoxFit.cover, imageUrl),
+          child: Image.network(
+            fit: BoxFit.cover,
+            imageUrl,
+            loadingBuilder: (context, child, loadingProgress) =>
+                loadingProgress == null
+                    ? child
+                    : ShimmerLoading(
+                        width: ScreenUtils.getScreenWidth(context) * 0.9,
+                        height: 100.0,
+                      ),
+          ),
         ));
   }
 }
