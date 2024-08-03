00dc63d2b60cdd7ac234c5f323d693ca4cdd416a
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-03 18:14:43 +0300

    add navigate from to product details

--- lib/features/favorite/presentation/screen/favorite_products_screen.dart
+++ lib/features/favorite/presentation/screen/favorite_products_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/core/router/app_navigator.dart';
 import 'package:e_commerce/features/favorite/presentation/widgets/favorite_product_card_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,29 +29,32 @@ class FavoriteProductsScreen extends StatelessWidget {
               style: AppTextStyle.font35BlackBold,
             ),
           ),
-          body: LayoutBuilder(
-            builder: (context, constraints) {
-              return SizedBox(
-                height: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.all(15.0),
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 1.0,
-                    crossAxisSpacing: 0.3,
-                    childAspectRatio: constraints.maxWidth > 400? 0.75 :1 / 1.75,
-                    children: List.generate(
-                      cubit.favoriteProducts.length,
-                      (index) => FavoriteProductCardItem(
+          body: LayoutBuilder(builder: (context, constraints) {
+            return SizedBox(
+              height: double.infinity,
+              child: Padding(
+                padding: const EdgeInsetsDirectional.all(15.0),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 1.0,
+                  crossAxisSpacing: 0.3,
+                  childAspectRatio:
+                      constraints.maxWidth > 400 ? 0.75 : 1 / 1.75,
+                  children: List.generate(
+                    cubit.favoriteProducts.length,
+                    (index) => GestureDetector(
+                      onTap: () => AppNavigator.navigateToProductDetails(
+                          context, (-1, cubit.favoriteProducts[index])),
+                      child: FavoriteProductCardItem(
                         favProduct: cubit.favoriteProducts[index],
                         showBottomSheet: cubit.showBottomSheet,
                       ),
                     ),
                   ),
                 ),
-              );
-            }
-          ),
+              ),
+            );
+          }),
         );
       },
     );
