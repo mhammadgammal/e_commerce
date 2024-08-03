59921786cc1dbd7b042673933da3a799cbee5d9e
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-03 12:22:37 +0300

    modify ChangeFavoriteUsecase to return a product model in addition to its recrord

--- lib/features/favorite/domain/usecase/change_favorite_usecase.dart
+++ lib/features/favorite/domain/usecase/change_favorite_usecase.dart
@@ -1,11 +1,12 @@
 import 'package:e_commerce/core/base_use_case/base_usecase.dart';
 import 'package:e_commerce/features/home/data/repositories/products_repository_impl.dart';
+import 'package:e_commerce/features/home/domain/entity/product_entity/product_model.dart';
 
-class ChangeFavoriteUsecase implements BaseUsecase<(bool, String?)> {
+class ChangeFavoriteUsecase implements BaseUsecase<(bool, String?, ProductModel?)> {
   final ProductsRepositoryImpl _productsRepositoryImpl;
   ChangeFavoriteUsecase(this._productsRepositoryImpl);
 
   @override
-  Future<(bool, String?)> perform(productId) async =>
+  Future<(bool, String?, ProductModel?)> perform(productId) async =>
       await _productsRepositoryImpl.changeFavourite(productId);
 }
